c0be3dbfc903ba9d61e93f0c0265525cfa4ec78f
ismaelp96 <ismael6991@live.com>
2024-03-08 17:35:15 -0300

    Create Navbar #4

---
+++ src/components/NavbarItem.jsx
@@ -0,0 +1,20 @@
+'use client';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+
+export default function NavbarItem({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
+
+  return (
+    <div
+      className={`font-semibold hover:text-amber-600 transition-all duration-200 ease-linear ${
+        genre === param
+          ? 'underline underline-offset-8 decoration-4 decoration-amber-600 rounded-lg'
+          : ''
+      }`}
+    >
+      <Link href={`/?genre=${param}`}>{title}</Link>
+    </div>
+  );
+}
