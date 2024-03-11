9801ce22a72e3e0890b150202df0a977dc752b6b
ismaelp96 <ismael6991@live.com>
2024-03-11 16:51:33 -0300

    Handle the Error #6

---
+++ src/app/error.jsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function Error({ err, reset }) {
+  useEffect(() => {
+    console.log(err);
+  }, [err]);
+  return (
+    <div className='text-center mt-10'>
+      <h1>Something went wrong. Please try again later.</h1>
+      <button
+        onClick={() => reset()}
+        className='hover:text-amber-600 transition-colors duration-300 ease-linear'
+      >
+        Try Again
+      </button>
+    </div>
+  );
+}
