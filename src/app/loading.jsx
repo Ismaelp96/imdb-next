f4c5c4603a7b5642e41a5d6821ead5ae6489fa6c
ismaelp96 <ismael6991@live.com>
2024-03-11 17:23:34 -0300

    Loading component #7

---
+++ src/app/loading.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default function Loading() {
+  return (
+    <div className='flex justify-center items-center'>
+      <Image
+        src='spinner.svg'
+        width={200}
+        height={200}
+        alt='loading'
+        title='loading'
+      />
+    </div>
+  );
+}
