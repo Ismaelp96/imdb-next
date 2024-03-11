96d66aeef6898f72924412f6ca6ef6815c1afd05
ismaelp96 <ismael6991@live.com>
2024-03-11 14:14:45 -0300

    Fetch data from TMDB API in the home page #5

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Header from '@/components/Header';
+import Header from '../components/Header';
 import Providers from './Providers';
-import Navbar from '@/components/NavBar';
+import Navbar from '../components/NavBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
