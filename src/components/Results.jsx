fc7772b035f9694b6a3f73bb43f7d07ae2a44134
ismaelbelmicro <ismael.patrick@belmicro.com.br>
2024-03-12 15:40:54 -0300

    Add card component #8

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,10 @@
+import CardComponent from './Card';
+
 export default function Results({ results }) {
   return (
-    <div>
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4 w-full'>
       {results.map((result) => (
-        <div key={result.id}>
-          <h2>{result.title}</h2>
-        </div>
+        <CardComponent key={result.id} result={result} />
       ))}
     </div>
   );
