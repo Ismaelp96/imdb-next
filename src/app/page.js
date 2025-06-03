655eaa97d079c19bf48a902999b70f87d52b95db
Ismaelp96 <ismael6991@live.com>
2025-06-03 12:24:02 -0300

    fix: url

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default async function Home({ searchParams }) {
 	const genre = searchParams.genre || 'fetchTrending';
 
 	const res = await fetch(
-		`https://api.themoviedb.org/3/${
+		`https://api.themoviedb.org/3${
 			genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
 		}?api_key=${API_KEY}&language=pt-BR&page=1`,
 		{
